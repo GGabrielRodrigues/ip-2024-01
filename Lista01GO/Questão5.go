b95e4a0f4ceb08405f4b29071f7963ba6276af99
GGabrielRodrigues <gabriel.rodrigues2345@discente.ufg.br>
2024-04-18 16:02:12 -0300

    Questão5

---
+++ Lista01GO/Questão5.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var (
+		conta int
+		gastagua, valorconta float64
+		tipocon string
+	)
+	
+	
+		  fmt.Print ("Insira o nº da conta: ")
+		  fmt.Scan(conta)
+		  fmt.Print ("Insira o consumo d'água em m³: ")
+		  fmt.Scan (gastagua)
+		  fmt.Print ("Insira o tipo de consumidor: ")
+		  fmt.Print (tipocon)
+		  fmt.Println (" ")
+		  if 
+		  tipocon == "R" {
+		  return (
+			valorconta == (5 + 0.05*gastagua)
+		    fmt.Println ("CONTA: ", conta)
+		    fmt.Print ("Preço a pagar: ",valorconta)
+		  )
+		  }
+		  else { 
+			(tipocon == "C") && (gastagua > 80) 
+			
+			  valorconta == 0.25*gastagua + 500
+			  Println ("CONTA: ", conta)
+			  Print ("Preço a pagar: ",valorconta)
+		  }
+		  else { 
+			(tipocon == "C") && (gastagua <= 80) 
+			  valorconta == 500
+			  Println ("CONTA: ", conta)
+			  Print ("Preço a pagar: ",valorconta:4:2)
+		}
+		  else {
+		   (tipocon = "I") && (gastagua <= 100) 
+			  valorconta == 800
+			  Println ("CONTA: ", conta)
+			  Print ("Preço a pagar: ",valorconta)
+		  }
+		  else {
+			 (tipocon = "I") e (gastagua > 100) entao
+			  valorconta <- gastagua*0.04 + 800
+			  Escreval ("CONTA: ", conta)
+			  Escreva ("Preço a pagar: ",valorconta:4:2)
+}
+
+}
